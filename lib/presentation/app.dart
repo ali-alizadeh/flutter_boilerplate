4b868e377b0d7c37b6fc0ed787b95ca27c7bb9e4
Ali Alizadeh <alizadeh.ali.e@gmail.com>
2020-12-05 13:26:23 -0500

    move MyHomePage to HomePage in pages folder, move MyApp to App in presentation

---
+++ lib/presentation/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'pages/home_page/home_page.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Boilerplate',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
