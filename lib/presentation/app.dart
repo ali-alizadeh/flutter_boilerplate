d8fcddb7ddcaac154e1c3ab96d1560b9b334b790
Ali Alizadeh <alizadeh.ali.e@gmail.com>
2020-12-06 05:01:30 -0500

    use app settings bloc and app theme in app widget

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../application/app_settings_bloc/app_settings_bloc.dart';
+import '../service_locator.dart';
 import 'l10n/app_localizations.dart';
 import 'pages/home_page/home_page.dart';
+import 'theme.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Boilerplate',
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: HomePage(),
+    return BlocBuilder<AppSettingsBloc, AppSettingsState>(
+      cubit: ServiceLocator.resolve<AppSettingsBloc>(),
+      builder: (context, state) {
+        return MaterialApp(
+          title: 'Flutter Boilerplate',
+          theme: AppTheme.light(isRtl: state.isRtl),
+          darkTheme: AppTheme.dark(isRtl: state.isRtl),
+          themeMode: state.themeMode,
+          locale: state.locale,
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          home: HomePage(),
+        );
+      },
     );
   }
 }
