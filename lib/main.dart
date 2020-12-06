f1434c5cfbbef8c583835862bcc4f4654192bbef
Ali Alizadeh <alizadeh.ali.e@gmail.com>
2020-12-06 04:43:59 -0500

    init hive in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 
 import 'application/app_bloc_observer.dart';
@@ -6,6 +8,7 @@ import 'presentation/app.dart';
 import 'service_locator.dart';
 
 Future<void> main() async {
+  await Hive.initFlutter();
   HydratedBloc.storage = await HydratedStorage.build();
   Bloc.observer = AppBlocObserver();
   configureServiceLocator();
