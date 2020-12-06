8b9f8217ba01451cd83ef84f2b28b53735abce9d
Ali Alizadeh <alizadeh.ali.e@gmail.com>
2020-12-06 04:46:06 -0500

    config flutter onError and wrap runApp with runZonedGaurded

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -6,11 +8,26 @@ import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'application/app_bloc_observer.dart';
 import 'presentation/app.dart';
 import 'service_locator.dart';
+import 'utils/log.dart';
 
 Future<void> main() async {
+  FlutterError.onError = (FlutterErrorDetails flutterErrorDetails) {
+    Log.e(
+      'Flutter error',
+      flutterErrorDetails,
+      flutterErrorDetails.stack,
+    );
+  };
+
   await Hive.initFlutter();
   HydratedBloc.storage = await HydratedStorage.build();
   Bloc.observer = AppBlocObserver();
   configureServiceLocator();
-  runApp(App());
+
+  runZonedGuarded<void>(
+    () => runApp(App()),
+    (Object object, StackTrace stackTrace) {
+      Log.e('Dart error', object, stackTrace);
+    },
+  );
 }
