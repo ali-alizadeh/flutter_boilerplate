cdb0fc50d6654993a46ecb9957d3998f93b0479c
Ali Alizadeh <alizadeh.ali.e@gmail.com>
2020-12-06 04:43:08 -0500

    config bloc and hydrated bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 
+import 'application/app_bloc_observer.dart';
 import 'presentation/app.dart';
 import 'service_locator.dart';
 
-void main() {
+Future<void> main() async {
+  HydratedBloc.storage = await HydratedStorage.build();
+  Bloc.observer = AppBlocObserver();
   configureServiceLocator();
   runApp(App());
 }
